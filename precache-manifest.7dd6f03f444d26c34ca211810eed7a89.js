3241db977a505a152a873498a1e01b1cfe16fe34
Yixin <yixin0325@gmail.com>
2020-04-20 23:11:50 -0400

    Updates

---
+++ precache-manifest.7dd6f03f444d26c34ca211810eed7a89.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97409fac7788dbbb3ecae6d1e74d8edf",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "0156f97b90fe002632b8",
+    "url": "/memory-game/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "47194d92c770748718ed",
+    "url": "/memory-game/static/css/main.9e130020.chunk.css"
+  },
+  {
+    "revision": "0156f97b90fe002632b8",
+    "url": "/memory-game/static/js/2.9ad0f78a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-game/static/js/2.9ad0f78a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47194d92c770748718ed",
+    "url": "/memory-game/static/js/main.0329410e.chunk.js"
+  },
+  {
+    "revision": "37eb60d43a600f236727",
+    "url": "/memory-game/static/js/runtime-main.38e44c2e.js"
+  },
+  {
+    "revision": "4958169d758bb2423faa6268ebd60d74",
+    "url": "/memory-game/static/media/card_back.4958169d.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/memory-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
